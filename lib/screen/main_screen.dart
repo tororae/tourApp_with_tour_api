580b1cc061a3dd8b8c3b5233d7e51e635e12344c
tororae <bsu98089@gmail.com>
2023-08-24 15:33:14 +0900

    뒤로가기 두번 클릭시 앱 종료 기능 추가

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,13 +1,35 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// ignore: depend_on_referenced_packages
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool backButtonChk = false; //뒤로가기 클릭여부 파악
+    ///뒤로가기 클릭시 Splash 화면으로 넘어가는것을 방지
+    ///더불어 2초 안에 2번 뒤로가기를 누르면 앱 종료 기능 추가
     return WillPopScope(
       onWillPop: () async {
+        if (backButtonChk == false) {
+          Fluttertoast.showToast(
+            msg: "한번 더 뒤로가기를 클릭하여 앱을 종료합니다.",
+          );
+          backButtonChk = true; //true로 설정한 후 뒤로가기 누르면 꺼짐
+          Future.delayed(
+            const Duration(milliseconds: 2000),
+            () {
+              backButtonChk = false;
+            },
+          );
+        } else {
+          SystemNavigator.pop(); //앱 종료함수
+        }
+
         return false;
       },
       child: const Scaffold(
--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -4,6 +4,8 @@ import 'package:tour_with_tourapi/setting/theme.dart';
 
 bool _isSplashClicked = false;
 
+//Splash 화면 실행 후 실행되는 타이머. 2초 후 메인화면으로 진입.
+//만약 클릭하여 화면을 넘어갔을 시, 이미 넘어간것을 확인하고 아무 조치를 취하지않음.
 void _goMainPageTimer(context) {
   Future.delayed(
     const Duration(milliseconds: 2000),
@@ -17,6 +19,7 @@ void _goMainPageTimer(context) {
   );
 }
 
+//2초 후 or 클릭시 메인 화면으로 넘어가는 함수
 void _goMainPage(context) {
   Navigator.push(
     context,
@@ -26,6 +29,7 @@ void _goMainPage(context) {
   );
 }
 
+//메인 화면으로 넘어가기 전 스플래시 화면임.
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
@@ -34,6 +38,7 @@ class SplashScreen extends StatelessWidget {
     _goMainPageTimer(context);
 
     return Scaffold(
+      //클릭시 화면 넘어가는 기능.
       body: GestureDetector(
         onTap: () {
           _goMainPage(context);
@@ -43,6 +48,7 @@ class SplashScreen extends StatelessWidget {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
+                //color는 theme.dart에 선언되어있음.
                 mainColor,
                 subColor,
               ],
