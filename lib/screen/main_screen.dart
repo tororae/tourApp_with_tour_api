531edcd5ad70c0671c2388d2a8bff5d1c101c8b2
tororae <bsu98089@gmail.com>
2023-08-27 16:30:41 +0900

    위치정보 획득 성공

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 // ignore: depend_on_referenced_packages
 import 'package:fluttertoast/fluttertoast.dart';
@@ -23,10 +28,95 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
+
+  List<Map<String, dynamic>> jsonData = [];
+  bool isLoading = false;
+  int page = 1;
+  Position? currentPosition;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    debugPrint("권한요청 간다.");
+    getCurrentLocation();
+  }
+
+  // final ScrollController _scrollController = ScrollController();
+
+  Future<void> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('위치 서비스를 활성화해주세요.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('위치 접근 권한을 허용해주세요.');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        throw Exception('위치 접근 권한을 허용해주세요.');
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      currentPosition = position;
+      debugPrint("$currentPosition");
+    });
+
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    if (isLoading) return;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    var latitude = currentPosition?.latitude ?? 0.0;
+    var longitude = currentPosition?.longitude ?? 0.0;
+
+    var url = Uri.parse(
+        'https://apis.data.go.kr/B551011/KorService1/locationBasedList1?serviceKey=p7lOrWvzLtnkrtjd%2Bq8KlJxffMsCCQQpgjq9o8Hi7Lo8aZrtnjYn9vPepxCYPudDUPTtGbQfsjfBI%2BAmSAx4lQ%3D%3D&numOfRows=10&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C&mapX=$longitude&mapY=$latitude&radius=1000&contentTypeId=32&pageNo=$page');
+
+    var res = await http.get(url);
+
+    if (res.statusCode == 200) {
+      var decodedData = jsonDecode(utf8.decode(res.bodyBytes));
+
+      if (decodedData['response']['header']['resultCode'] == "0000") {
+        var itemList = decodedData['response']['body']['items']['item'];
+
+        if (itemList is List) {
+          var itemListTyped = itemList.cast<Map<String, dynamic>>();
+
+          setState(() {
+            jsonData.addAll(itemListTyped);
+            page++;
+            isLoading = false;
+          });
+        } else {
+          throw Exception('데이터 형식이 올바르지 않습니다.');
+        }
+      } else {
+        throw Exception('데이터를 불러오는 데 실패했습니다.');
+      }
+    } else {
+      throw Exception('데이터를 불러오는 데 실패했습니다.');
+    }
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      debugPrint("$index가 선택됨.");
     });
   }
 
