ea30b0ba71de8a50a6211464092e33b1da143992
tororae <bsu98089@gmail.com>
2023-08-27 15:23:19 +0900

    merge 중 발생한 에러 처리

--- lib/screen/schedule_screen.dart
+++ lib/screen/schedule_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tour_with_tourapi/setting/theme.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 
 class ScheduleScreen extends StatefulWidget {
