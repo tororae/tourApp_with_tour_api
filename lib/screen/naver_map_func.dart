1e94abcab258367642e07d22acf1a687af1d7735
tororae <bsu98089@gmail.com>
2023-09-01 22:29:53 +0900

    지도 클릭시 좌표 얻어오기 성공

--- lib/screen/naver_map_func.dart
+++ lib/screen/naver_map_func.dart
@@ -1,7 +1,11 @@
 // ignore: unused_import
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:tour_with_tourapi/setting/secret.dart';
 import 'package:tour_with_tourapi/setting/theme.dart';
 
 Position getPosition = currentPosition ??
@@ -17,7 +21,6 @@ Position getPosition = currentPosition ??
     );
 
 NaverMap naverMapTest(context) {
-  NaverMapController myMapController;
   debugPrint("위도 : ${getPosition.latitude}, 경도 : ${getPosition.longitude}");
   return NaverMap(
     options: NaverMapViewOptions(
@@ -34,7 +37,41 @@ NaverMap naverMapTest(context) {
       debugPrint("네이버 맵 로딩됨!");
     },
     onMapTapped: (point, latLng) {
-      Navigator.pop(context);
+      // Navigator.pop(context);
+      debugPrint("${latLng.latitude}、${latLng.longitude}");
+      getAddress("${latLng.longitude},${latLng.latitude}");
     },
   );
+}
+
+//동변환 함수
+Future<void> getAddress(position) async {
+  const String apiUrl =
+      "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc";
+  String coords = position; // 여기에 입력 좌표 값을 넣으세요
+  const String orders = "legalcode"; // 여기에 변환 작업 이름 값을 넣으세요
+  const String output = "json"; // 여기에 출력 형식 값을 넣으세요
+  const String apiKeyId = naverMapApiKey;
+  const String apiKeySecret = naverMapApiSecret;
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+      Uri.parse("$apiUrl?coords=$coords&orders=$orders&output=$output"),
+      headers: {
+        "X-NCP-APIGW-API-KEY-ID": apiKeyId,
+        "X-NCP-APIGW-API-KEY": apiKeySecret,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // JSON 응답 파싱
+      final Map<String, dynamic> data = json.decode(response.body);
+      debugPrint("Response Data: $data");
+    } else {
+      // 요청 실패 처리
+      debugPrint("Failed to load data: ${response.statusCode}");
+    }
+  }
+
+  fetchData();
 }
